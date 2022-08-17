59eb0acae6ba65df1275e5bebe48ebf3f0a1dc8e
Trogiken <noah@blaszak.com>
2022-08-17 11:31:23 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='gitUpdater',
+    version='0.1.0',
+    description='Utilize github to update client programs',
+    url='https://github.com/Trogiken/pyexample',
+    author='Noah Blaszak',
+    author_email='noah.blaszak@gmail.com',
+    license='GPL-3.0',
+    packages=['updater'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Anyone',
+        'License :: GPL License',
+        'Programming Language :: Python :: 3.6+',
+    ],
+)
