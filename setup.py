c0319e89e8c109f6217d8ecf87aae5b6972bd383
Trogiken <noah@blaszak.com>
2022-08-19 00:41:50 -0400

    Organized functions, exceptions, and variables

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='gitUpdater',
-    version='0.1.0',
+    version='0.1.1',
     description='Utilize github to update client programs',
     url='https://github.com/Trogiken/pyexample',
     author='Noah Blaszak',
--- updater/main.py
+++ updater/main.py
@@ -3,7 +3,7 @@ import requests
 
 
 class _Repo:
-    """Gather github repository information"""
+    """Gather GitHub repository information"""
     def __init__(self, username: str, repository: str, token: str):
         self.user = username
         self.repo = repository
@@ -15,25 +15,40 @@ class _Repo:
 
     def fetch(self, url) -> dict:
         """Return all repository information"""
-        status = None
-        message = 'Unknown'
         try:
             resp = self.session.get(url, headers=self.auth_header if self.token is not None else None)
-            if not resp.ok:
-                status = False
-                message = f"Failed to get '{self.repo_url}'. Reason: '{resp.reason}' (Private repos require tokens)"
-            else:
+            if resp.ok:
                 status = True
                 message = resp.json()
+            else:
+                status = False
+                message = f"Failed to get '{self.repo_url}'. Reason: '{resp.reason}' (Is this a private repo?)"
         except requests.exceptions.ConnectionError:
-            status = False
-            message = 'Failed to make connection'
+            raise ConnectionError("Failed to make connection!")
 
         return {'ok': status, 'msg': message}
 
-    def get(self, param: str) -> fetch:
-        """Return specific repository url information"""
-        return self.fetch(f'{self.repo_url}/{param}')
+    def get_versions(self) -> list:
+        """List tag versions in ascending order"""
+        response = self.fetch(f'{self.repo_url}/tags')
+        if not response['ok']:
+            raise LookupError(response['msg'])
+        else:
+            tags_data = response['msg']
+
+        versions = []
+        for tag in tags_data:
+            v = tag['name'].lower()
+            v = v if v[0] != 'v' else v[:0] + v[0+1:]  # remove 'v' from version if exists at index 0
+            versions.append(v)
+        versions.sort(key=version.Version)  # ascend order
+
+        return versions
+
+
+class _Disk:
+    """Manage disk"""
+    pass
 
 
 class Update(_Repo):
@@ -43,31 +58,19 @@ class Update(_Repo):
     Attributes
     ----------
     username:
-        github username
+        GitHub username
     repository:
         repository name
     token (optional):
-        github personal access token
+        GitHub personal access token
     """
     def __init__(self, username: str, repository: str, token: str = None):
         super().__init__(username, repository, token)
 
-    def check(self, current):
+    def check(self, current) -> bool:
+        latest = self.get_versions()[-1]
 
-        response = self.get('tags')
-        if not response['ok']:
-            raise Exception(f"Error: {response['msg']}")
-        else:
-            tag_data = response['msg']
-
-        versions = []
-        for tag in tag_data:
-            v = tag['name'].lower()
-            v = v if v[0] != 'v' else v[:0] + v[0+1:]  # remove common 'v' from version if exists at index 0
-            versions.append(v)
-        versions.sort(key=version.Version)  # versions in ascending order
-
-        if version.parse(versions[-1]) > version.parse(current):
+        if version.parse(latest) > version.parse(current):
             return True
         else:
             return False
